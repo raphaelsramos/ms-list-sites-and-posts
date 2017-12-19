8a784b0ec4f6d69359d732c896d1c740d8fd6539
Raphael Ramos <chappados@gmail.com>
2017-12-19 11:33:51 -0200

    Update ms-list-sites-and-posts.php

--- ms-list-sites-and-posts.php
+++ ms-list-sites-and-posts.php
@@ -2,37 +2,32 @@
 
 /*
 Plugin Name: MS List Site & Posts
-Plugin URI: http://www.raphaelramos.com.br/wp/plugins/ms-list-site-and-posts/
+Plugin URI: https://github.com/raphaelsramos/ms-list-sites-and-posts/
 Description: Trying associate posts between Multisites? When activated, this plugins show a list of sites in the network. When the site is selected, it shows the list of posts (of same post type) for association.
 Author: Raphael Ramos
 Author URI: http://www.raphaelramos.com.br/
-Text Domain: ms-sites-p2p
+Text Domain: ms-lsap
 Domain Path: /lang/
-Version: 0.21
-Date: 2015-02-28
+Version: 1.0.1
+Date: 2017-12-19
 */
 
 	define( 'LSAP_ROOT', dirname( __FILE__ ) .'/' );
 
-	include( 'lsap.widget.php' );
-	include( 'lsap.options.php' );
-	include( 'lsap.helpers.php' );
+	require_once 'metabox.php';
+	require_once 'options.php';
+	require_once 'helpers.php';
 
 	/***
 	 * Calls the class on the post edit screen.
 	 */
-	function call_LSAP_Widget(){
-		new LSAP_Widget();
+	function LSAP_add_metabox(){
+		new LSAP_Metabox();
 	}
 
 	new LSAP_Options();
 
 	if( is_admin() ){
-		add_action( 'load-post.php', 'call_LSAP_Widget' );
-		add_action( 'load-post-new.php', 'call_LSAP_Widget' );
+		add_action( 'load-post.php', 'LSAP_add_metabox' );
+		add_action( 'load-post-new.php', 'LSAP_add_metabox' );
 	}
-
-	
-	
-	
-	
